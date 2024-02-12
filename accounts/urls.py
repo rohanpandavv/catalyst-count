2115d139a44420ba63c5a76e04d97609ff1398fb
Rohan Pandav <rohanpandav9@gmail.com>
2024-02-12 14:00:18 +0530

    📦 NEW: add user listing & add logic
    Add logic for add user and list user
    Add templates

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,5 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', include('allauth.urls'))
+    path('', include('allauth.urls')),
+    path('list/', views.list_users, name='list_users'),
+    path('add/', views.AddUserView.as_view(), name='add_user'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+def list_users(request):
+    users = User.objects.all()
+    return render(request, 'users/users_list.html', {'users': users})
+
+class AddUserView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'users/add_user.html'
+    success_url = reverse_lazy(list_users)
