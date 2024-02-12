52e077d8284941ec3752a63b40cf6a4543ea1929
Rohan Pandav <rohanpandav9@gmail.com>
2024-02-12 14:43:56 +0530

    📦 NEW: Add rest_framework and query_builder
    To installed Applications

--- catalyst_count/settings.py
+++ catalyst_count/settings.py
@@ -46,7 +46,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bootstrap5',
-    'data_uploader'
+    'data_uploader',
+    'query_builder',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -145,6 +147,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
